064ae4d2933e0514f5dd7d9246f363ce94d4b421
Alex Carstensen <matthacker.alex@gmail.com>
2016-09-28 21:41:07 +0200

    implemented write method in Bitmap class

--- Bitmap.cpp
+++ Bitmap.cpp
@@ -3,20 +3,34 @@
 //
 
 #include "Bitmap.h"
+#include "BitmapFileHeader.h"
+#include "BitmapInfoHeader.h"
 
-
+using namespace bitmapSanity;
 namespace bitmapSanity
 {
 
     Bitmap::Bitmap(int width, int height)
-            : width_(width), height_(height), pixels_(new uint8_t[width * height * 3])
+            : width_(width), height_(height),
+              pixels_(new uint8_t[width * height * 3]{}) // initialize all to zero with {}
     {}
 
     Bitmap::~Bitmap()
     {}
 
     bool Bitmap::write(std::string filename)
-    { return false; }
+    {
+        BitmapFileHeader fileHeader;
+        BitmapInfoHeader infoHeader;
+
+        fileHeader.fileSize = sizeof(BitmapFileHeader) + sizeof(BitmapInfoHeader) + width_ * height_ * 3;
+        fileHeader.dataOffset = sizeof(BitmapFileHeader) + sizeof(BitmapInfoHeader);
+
+        infoHeader.width = width_;
+        infoHeader.height = height_;
+
+        return false;
+    }
 
     void Bitmap::setPixel(int x, int y, uint8_t red, uint8_t blue, uint8_t green)
     {
